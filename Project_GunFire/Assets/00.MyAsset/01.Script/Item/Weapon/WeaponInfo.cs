de59ec697dbb6ba1ba2fdf5a3a0da8a7417341ae
dozerlourd <54570921+dozerlourd@users.noreply.github.com>
2021-07-13 22:41:56 +0900

    Revert "07/13 - Merge Rapa Branch"
    This reverts commit aa61bc77bfd59f3d3129299d025ac79bdf3da557, reversing
    changes made to f4e611562d5b9bb4c99b2b76ae9263baaa7725b7.

--- Project_GunFire/Assets/00.MyAsset/01.Script/Item/Weapon/WeaponInfo.cs
+++ Project_GunFire/Assets/00.MyAsset/01.Script/Item/Weapon/WeaponInfo.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponInfo : ItemInfo
+public class WeaponInfo : MonoBehaviour
 {
     [Tooltip("현재 무기의 무기 번호 (WeaponSystem의 무기 번호 참조)"), SerializeField] int _weaponNum;
     public int WeaponNum => _weaponNum;
 
-    protected override float GravityScale()
-    {
-        return 0;
-    }
+    //public Weapon thisWeapon;
 
-    protected override Vector3 GravityDirection()
-    {
-        return Vector3.down;
-    }
+    //private void Start()
+    //{
+    //    thisWeapon = WeaponSystem.Instance.ChangeWeapon(_weaponNum);
+    //}
 }
